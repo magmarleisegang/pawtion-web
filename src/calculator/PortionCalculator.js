946e9e0d328fe17b35cb65b3256b9ecde30aadc6
magdalena.leisegang <magdalena.leisegang@entelect.co.za>
2020-05-01 22:24:02 +0200

    remove portionCost from state and calculate on render

--- src/calculator/PortionCalculator.js
+++ src/calculator/PortionCalculator.js
@@ -33,7 +33,6 @@ export default class PortionCalculator extends React.Component {
             bag: 0.0,
             price: 0.0,
             portionSize: 0.0,
-            portionCost: 0.0,
             Text: {
                 bag: "0",
                 price: "0",
@@ -50,41 +49,32 @@ export default class PortionCalculator extends React.Component {
             clear: "<",
             zeros: ".00"
         }
-
-        this.maintainZero = false;
     }
 
 
 
     _calculatePortionCost() {
-        console.log("calculate portion cost");
         if (this.state.price > 0
             && this.state.bag > 0
             && this.state.portionSize > 0) {
-            const pricePerGram = this.state.price / (this.state.bag * 1000);
-            console.log("price per gram: " + pricePerGram);
-            const pricePerPortion = pricePerGram * this.state.portionSize;
-            this.setState({ portionCost: pricePerPortion });
+
+            const mealsPerBag = this.state.bag * 1000.00 / this.state.portionSize;
+            const pricePerMeal = this.state.price / mealsPerBag;
+            return pricePerMeal.toFixed(2);
         }
+        return "0.00"
     }
 
     _calculatorButtonPressed(char) {
-        console.log(char);
         let currentText = this.getCurrentEditingText();
-
-        console.log(`current value: ${currentText}`);
 
         if (char === this.actions.clear) {
             currentText = currentText.substring(0, currentText.length - 1)
-        }
-        else if (char === this.actions.zeros) {
+        } else if (char === this.actions.zeros) {
             currentText += "00";
-        }
-        else {
+        } else {
             currentText += char;
-
         }
-        console.log(`new current value: ${currentText}`);
 
         this.setCurrentEditingValue(currentText);
     }
@@ -93,45 +83,41 @@ export default class PortionCalculator extends React.Component {
         console.log(this.isEditing)
         switch (this.isEditing) {
             case "bag":
-                return this.state.Text.bag;
+                return this.state.Text.bag || "0";
             case "price":
-                return this.state.Text.price;
+                return this.state.Text.price || "0";
             case "portion":
-                return this.state.Text.portionSize;
+                return this.state.Text.portionSize || "0";
             default:
                 break;
         }
-
     }
 
     setCurrentEditingValue(val) {
-
         let newFloatValue = parseFloat(val) / 100;
+
         switch (this.isEditing) {
             case "bag":
-
                 this.setState({ bag: newFloatValue, Text: { bag: val } });
                 break;
             case "price":
                 this.setState({ price: newFloatValue, Text: { price: val } });
-
                 break;
             case "portion":
                 this.setState({ portionSize: newFloatValue, Text: { portionSize: val } });
-
                 break;
             default:
                 break;
         }
-        this.calculatePortionCost();
     }
 
     _rememberMe() {
+        const  portionCost = parseFloat(this.calculatePortionCost())
         const currentValue = {
             bag: this.state.bag,
             price: this.state.price,
             portionSize: this.state.portionSize,
-            portionCost: this.state.portionCost
+            portionCost: portionCost
         };
         if (this.props.onRememberMe)
             this.props.onRememberMe(currentValue);
@@ -143,7 +129,7 @@ export default class PortionCalculator extends React.Component {
             <View style={{ height: '90vh' }}>
                 <View style={{ flex: 1 }}>
                     <Text>Price/Portion</Text>
-                    <Text style={{ fontSize: '2.5em' }}>{this.state.portionCost.toFixed(2)}</Text>
+                    <Text style={{ fontSize: '2.5em' }}>{this.calculatePortionCost()}</Text>
                 </View>
                 <View style={{ flex: 0.5 }}>
                     <Button onPress={this.rememberMe}
