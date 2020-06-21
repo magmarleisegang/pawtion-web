ee83b9efdb567a923bb7f2b5b69fc202d093810b
magmarleisegang <magmarleisegang@gmail.com>
2020-06-21 21:11:37 +0200

    Add "config" to table component
    Used to set up which properties of an object to display and what to use as column header

--- src/portions/PTable.js
+++ src/portions/PTable.js
@@ -12,20 +12,20 @@ export default class PTable extends React.Component {
         return (
             <View key={dataObject.key} style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }}>
                 {
-                    Object.values(dataObject).map((val, idx) => {
-                        return this.renderCell(val, `${dataObject.key}-${idx}`)
+                    this.props.config.map((val, idx) => {
+                        return this.renderCell(dataObject[val.propertyName] , `${dataObject.key}-${idx}`)
                     })
                 }
             </View>
         );
     }
 
-    renderHeaderRow(dataObject) {
+    renderHeaderRow(configObject) {
         return (
             <View key="header" style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }}>
                 {
-                    Object.getOwnPropertyNames(dataObject).map((val, idx) => {
-                        return this.renderCell(val, `header-${idx}`)
+                    configObject.map((val, idx) => {
+                        return this.renderCell(val.displayName, `header-${val.propertyName}`)
                     })
                 }
             </View>
@@ -35,10 +35,9 @@ export default class PTable extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                {this.renderHeaderRow(this.props.data[0])}
+                {this.renderHeaderRow(this.props.config)}
                 {
                     this.props.data.map((datum) => { // This will render a row for each data element.
-                        console.log(datum)
                         return this.renderRow(datum);
                     })
                 }
--- src/portions/PortionList.js
+++ src/portions/PortionList.js
@@ -4,21 +4,37 @@ import { Text } from 'react-native';
 export default class PortionList extends React.Component{
     constructor(props) {
         super(props);
-        // props.header = ["Name", "Pawtion"];
         this.state = {list: props.list || []}
+        this.portionListConfig = [
+            {
+                displayName: "Name",
+                propertyName: "name"
+            },
+            {
+                displayName: "Weight",
+                propertyName: "bag"
+            },
+            {
+                displayName: "Portion Cost",
+                propertyName: "portionCost"
+            },
+            {
+                displayName: "Price",
+                propertyName: "price"
+            }
+        ]
     }
     addToList(pawtion)
     {
         const newList = this.state.list.concat(pawtion);
-        console.log(newList)
         this.setState({list: newList});
-
     }
     render(){
         return (
             <>
             <Text>Portion List</Text>
-            <Table data={this.state.list}/>
+            <Table data={this.state.list}
+             config={this.portionListConfig}/>
             </>
         );
     }
